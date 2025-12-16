28c8a98b97b2c9f6e2a11d1f09edda760214c3f3
Morena <morenicora@gmail.com>
2025-12-16 23:32:51 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'your', 'a']
+  let adj = ['great', 'big', 'cute', 'magestic']
+  let noun = ['duck', 'racoon', 'bunny', 'rainbow', 'unicorn']
+  let extensions = ['.com', '.net', '.us', '.io', '.es']
+
+  for (var p = 0; p < pronoun.length; p++) {
+    for (var a = 0; a < adj.length; a++) {
+      for (var n = 0; n < noun.length; n++) {
+        for (var e = 0; e < extensions.length; e++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + extensions[e])
+        }
+      }
+    }
+  }
+}
+
+
+
